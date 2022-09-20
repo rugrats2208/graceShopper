20f362249d58dcaeaf1789a4f1feddd946ef24d5
nbenford <noel.benford@gmail.com>
2022-09-20 15:12:59 -0400

    updated User model with auth methods. incomplete

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,62 +1,114 @@
 const conn = require('../conn');
 const { Sequelize } = conn;
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+//hash salting rounds
+const SALT_ROUNDS = 5;
 
 const User = conn.define('user', {
   username: {
     type: Sequelize.STRING,
-    allowNull: false,
     unique: true,
+    allowNull: false,
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   email: {
     type: Sequelize.STRING,
     unique: true,
+    allowNull: false,
   },
   fName: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   lName: {
     type: Sequelize.STRING,
+    allowNull: false,
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
   },
 });
 
-//create authentication
-User.prototype.generateToken = async function () {
-  try { }
-  catch (err) {
+//AUTHENTICATION
 
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function (candidatePwd) {
+  //we need to compare the plain version to an encrypted version of the password
+  return bcrypt.compare(candidatePwd, this.password);
+};
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
+
+/**
+ * classMethods
+ */
+User.authenticate = async function ({ username, password }) {
+  const user = await this.findOne({ where: { username } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password');
+    error.status = 401;
+    throw error;
   }
-}
+  return user.generateToken();
+};
 
 User.findByToken = async function (token) {
-  try { }
-  catch (err) {
-
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
+    if (!user) {
+      throw 'nooo';
+    }
+    return user;
+  } catch (ex) {
+    const error = Error('bad token');
+    error.status = 401;
+    throw error;
   }
-}
+};
 
-User.authenticate = async function (user) {
-  try { }
-  catch (err) {
-
+/**
+ * hooks
+ */
+const hashPassword = async (user) => {
+  //in case the password has been changed, we want to encrypt it with bcrypt
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
   }
-}
+};
 
+User.beforeCreate(async (user, options) => {
+  const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+  user.password = hashedPassword;
+});
+User.beforeUpdate(async (user, options) => {
+  const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+  user.password = hashedPassword;
+});
 
-
-User.prototype.getCart = async function () { };
+//CART
+User.prototype.getCart = async function () {};
 
 User.prototype.addToCart = async function () {
   //grab the order associated with the user
   //orders are your cart
 };
 
-User.prototype.removeFromToCart = async function () { };
+User.prototype.removeFromToCart = async function () {};
 
 //convert order model from cart to actual placed order
-User.prototype.createOrder = async function () { };
+User.prototype.createOrder = async function () {};
 
 module.exports = User;
