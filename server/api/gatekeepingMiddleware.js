749bea9037f6da5d9d57952c07a14aa42b0b14e5
nbenford <noel.benford@gmail.com>
2022-09-20 15:09:11 -0400

    gatekeeping middleware for auth routes

---
+++ server/api/gatekeepingMiddleware.js
@@ -0,0 +1,19 @@
+const {
+  models: { User },
+} = require('../db/');
+//store all of our functions that will act as middleware between our req and res
+
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  requireToken,
+};
