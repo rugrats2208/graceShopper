0cbdfc54a811c705bda5d837f573e95d7ca5afb4
Tenzing Sherpa <tsherpa2016@gmail.com>
2022-09-20 12:28:47 -0400

    testing pushing to local branch

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AllAlbums, AllPlaylists } from './';
+
+function App() {
+  return (
+    <div>
+      <AllAlbums />
+      <AllPlaylists />
+    </div>
+  );
+}
+
+export default App;
