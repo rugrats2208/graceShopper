644d998011f8351b7c18d51df6013bb577e6ae8b
Andrew Dobson <tandrewlopez@gmail.com>
2022-09-20 13:45:38 -0400

    Reorganized db folder. Models are now located inside of a model folder. Fixed import issues for affected items.

--- server/db/index.js
+++ server/db/index.js
@@ -2,11 +2,11 @@ const conn = require('./conn');
 const { Sequelize } = conn;
 
 //MODELS
-const User = require('./User');
-const Order = require('./Order');
-const Product = require('./Product');
-const Tag = require('./Tag');
-const LineItem = require('./LineItem'); //line item is the product and the amount of that product to buy
+const User = require('./models/User');
+const Order = require('./models/Order');
+const Product = require('./models/Product');
+const Tag = require('./models/Tag');
+const LineItem = require('./models/LineItem'); //line item is the product and the amount of that product to buy
 
 
 //ASSOCIATIONS
