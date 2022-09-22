cf267e5cef42ec6a5456b1b439220599a20f810d
Tenzing Sherpa <tsherpa2016@gmail.com>
2022-09-22 08:51:45 -0400

    Added working new/featured albums components

--- src/components/albums/FeaturedAlbums.jsx
+++ src/components/albums/FeaturedAlbums.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Album from './Album';
-
+import { useSelector } from 'react-redux';
 function FeaturedAlbums() {
-  function filterFeaturedAlbums() {
-    //check price of albums
-    //get the 30 most expensive and return
+  const albums = useSelector((state) => state.albums);
+  const [filteredAlbums, setFilteredAlbums] = useState([]);
+
+  //getting 30 most expensive albums (to serve as featured for time being)
+  function filterFeaturedAlbums(albumArray) {
+    //using a new array that will be to not manipulate the array being passed in (which will be the albums array(the global album state))
+    let featuredAlbums = [];
+    for (let i = 0; i < albumArray.length; i++) {
+      featuredAlbums.push(albumArray[i]);
+    }
+    featuredAlbums.sort((album1, album2) => {
+      return album2.price - album1.price;
+    });
+    return featuredAlbums.slice(0, 30);
   }
+
+  useEffect(() => {
+    setFilteredAlbums(filterFeaturedAlbums(albums));
+  }, [albums]);
+
+  //console.log(filteredAlbums);
   return (
     <div className="featured-albums">
       <h1>Featured Albums</h1>
       <div className="featured-albums-list">
-        {featuredAlbums.map((album) => (
+        {filteredAlbums.map((album) => (
           <Album key={album.id} data={album} />
         ))}
       </div>
