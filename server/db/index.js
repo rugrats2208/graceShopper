a47e226145dfda7dccb7baad7fdb9728fff33609
Zach Byerley <zachbyerley@gmail.com>
2022-09-22 16:32:15 -0400

    meta(server/db/index): removed comments and unused vars

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,4 @@
 const conn = require('./conn');
-const { Sequelize } = conn;
 
 //GET DATA FROM SPOTIFY API
 const { getAlbumData, getArtist } = require('./grabAlbums');
@@ -31,7 +30,6 @@ const syncAndSeed = async () => {
         //WITH FORCE TRUE ENABLED, THE DATABASE WILL DROP THE TABLE BEFORE CREATING A NEW ONE
         console.log('Started Seeding...');
         await conn.sync({ force: true });
-        // await conn.sync({ force: false });
 
         //LOADING USERS
         const users = await getUsers();
