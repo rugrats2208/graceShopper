440eba222b8c4def3c857fd6c6226b17e0ae7f8a
Tenzing Sherpa <tsherpa2016@gmail.com>
2022-09-22 19:58:58 -0400

    Fixed small navbar bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,6 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../reducers/Auth/authReducer';
 import Signup from '../auth/Signup';
 
-
 function Navigation() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -24,43 +23,42 @@ function Navigation() {
   //login  handler
   function handleLogin(e) {
     e.preventDefault();
-    navigate("/signup");
+    navigate('/signup');
   }
   //logout  handler
   function handleLogout(e) {
     e.preventDefault();
     dispatch(logout());
-    navigate("/");
+    navigate('/');
     window.location.reload();
   }
 
   return (
     <>
       {showLog && <Signup show={showLog} onHide={handleCloseLog} />}
-      <Navbar className='shadow' bg='dark' variant='dark' fixed='top'>
-
+      <Navbar className="shadow" bg="dark" variant="dark" fixed="top">
         <Container fluid>
           <Navbar.Brand href="#">Grace Shopper</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
+              style={{ maxHeight: '100px' }}
               navbarScroll
             >
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/allAlbums">Products</Nav.Link>
+              <Nav.Link href="/allProducts">Products</Nav.Link>
               <Nav.Link href="/admin">Admin</Nav.Link>
             </Nav>
 
             <Navbar.Text>
-              Signed in as: <a href="#login">{username || "guest"} </a>
+              Signed in as: <a href="#login">{username || 'guest'} </a>
             </Navbar.Text>
 
             {username === null && (
               <Button
-                className='ms-3'
-                variant='outline-success'
+                className="ms-3"
+                variant="outline-success"
                 onClick={handleShowLog}
               >
                 Sign In
