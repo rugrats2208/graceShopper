0441b22b62ebf1ad0ac59301f19a1639f85f25c2
nbenford <noel.benford@gmail.com>
2022-09-23 14:13:26 -0400

    redux-persist set up for auth

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,7 @@
 import { combineReducers } from 'redux';
-import albumsReducer from './albums/albumsReducer';
-import singleAlbumsReducer from './albums/singleAlbumReducer';
-import singleArtistReducer from './artists/singleArtistReducer';
+
 import authReducer from './Auth/authReducer';
 
 export default combineReducers({
-  albums: albumsReducer,
-  singleAlbum: singleAlbumsReducer,
-  singleArtist: singleArtistReducer,
   auth: authReducer,
 });
