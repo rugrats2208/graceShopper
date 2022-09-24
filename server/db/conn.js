fe6a607c34c8f136deb4db2447778e4adc42fa29
Andrew Dobson <tandrewlopez@gmail.com>
2022-09-24 14:19:11 -0400

    added ssl config

--- server/db/conn.js
+++ server/db/conn.js
@@ -3,6 +3,15 @@ const Sequelize = require('sequelize');
 const config = {
   "logging": false
 };
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
 
 //TEMPORARILY ASSIGNED DATABASE NAME TO "grace-shopper"
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/grace-shopper', config);
