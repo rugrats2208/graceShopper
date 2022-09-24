6d08bf5d05f73ce33f18358a66de8e4e1d2101dc
Zach Byerley <zachbyerley@gmail.com>
2022-09-24 04:28:23 -0400

    feat(api/shop): backend route for orders

--- server/api/shop.js
+++ server/api/shop.js
@@ -5,84 +5,90 @@ const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 // GET api/shop
 //TODO: change price and track length to human readable here
 router.get('/', async (req, res, next) => {
-  try {
-    const data = await Product.findAll({
-      include: Artist,
-    });
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+    try {
+        const data = await Product.findAll({
+            include: Artist,
+        });
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
-// GET api/album/:id
+// GET api/shop/album/:id
 router.get('/album/:id', async (req, res, next) => {
-  try {
-    const data = await Product.findByPk(req.params.id, {
-      include: [Track, Artist],
-    });
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+    try {
+        const data = await Product.findByPk(req.params.id, {
+            include: [Track, Artist],
+        });
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
-// GET api/artist/:id
+// GET api/shop/artist/:id
 router.get('/artist/:id', async (req, res, next) => {
-  try {
-    const data = await Artist.findByPk(req.params.id, {
-      include: Product,
-    });
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+    try {
+        const data = await Artist.findByPk(req.params.id, {
+            include: Product,
+        });
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
 // CART PATHS
-//TODO: write backend routes
-// GET api/order/:id
-router.get('/order/:id', async (req, res, next) => {
-  try {
-    res.send('hi');
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+// GET api/shop/order/:userId
+router.get('/order/:userId', async (req, res, next) => {
+    try {
+        const data = await Order.findAll({
+            where: { userId: req.params.userId },
+            include: {
+                model: Product,
+                include: Artist,
+            },
+        });
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
 //ADMIN PATHS
 router.post('/albums', requireToken, isAdmin, async (req, res, next) => {
-  try {
-    const { name, price, qty } = req.body;
-    const product = await Product.create({ name, price, qty });
-    res.send(product);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const { name, price, qty } = req.body;
+        const product = await Product.create({ name, price, qty });
+        res.send(product);
+    } catch (error) {
+        next(error);
+    }
 });
 
 //TODO: GET PRODUCT FROM DB AND UPDATE WITH NEW INFORMATION
 router.put('/albums/:id', requireToken, isAdmin, async (req, res, next) => {
-  try {
-    console.log('in the put route');
-    res.send('something');
-  } catch (error) {
-    next(error);
-  }
+    try {
+        console.log('in the put route');
+        res.send('something');
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.delete('/albums/:id', requireToken, isAdmin, async (req, res, next) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
-    await product.destroy();
-    res.send(product);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const product = await Product.findByPk(req.params.id);
+        await product.destroy();
+        res.send(product);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
