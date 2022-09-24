3dbb19dee41adc5ec52a61ce4dd2517aab61b5b0
Andrew Dobson <tandrewlopez@gmail.com>
2022-09-24 12:04:18 -0400

    added page not found component and route.

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="pageNotFound">
+      <h1>Page Not Found</h1>
+      <h2>404</h2>
+
+      <p>
+        The Page you are looking for doesn't exist or an other error occured.
+      </p>
+    </div>
+  );
+};
+
+export default PageNotFound;
