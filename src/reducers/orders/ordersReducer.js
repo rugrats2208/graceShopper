4affe122a7a06d78cbe09ae0623bb41266833c50
Zach Byerley <zachbyerley@gmail.com>
2022-09-27 01:05:42 -0400

    feat: custom scrollbars

--- src/reducers/orders/ordersReducer.js
+++ src/reducers/orders/ordersReducer.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const token = window.localStorage.getItem('token');
 /*
 TODO: refactor to have active order
     -initial state = {activeOrder: {}, pastOrders: []}
@@ -12,100 +13,99 @@ const DEL_ORDER_ITEM = 'DEL_ORDER_ITEM';
 const CHANGE_QTY = 'CHANGE_QTY';
 
 //ACTION CREATOR
-const setOrders = (orders) => ({
-  type: SET_ORDER,
-  orders,
+const setOrders = orders => ({
+    type: SET_ORDER,
+    orders,
 });
 
-const addItem = (item) => ({
-  type: ADD_ORDER_ITEM,
-  item,
+const addItem = item => ({
+    type: ADD_ORDER_ITEM,
+    item,
 });
 
-const deleteItem = (itemId) => ({
-  type: DEL_ORDER_ITEM,
-  itemId,
+const deleteItem = itemId => ({
+    type: DEL_ORDER_ITEM,
+    itemId,
 });
 
 const updateQty = (itemId, num) => ({
-  type: CHANGE_QTY,
-  itemId,
-  num,
+    type: CHANGE_QTY,
+    itemId,
+    num,
 });
 
 //THUNKS
-export const getOrders = (userId) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-
-      const { data } = await axios.get(`/api/shop/orders/${userId}`, {
-        headers: {
-          authorization: token,
-        },
-      });
-      dispatch(setOrders(data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-export const addOrderItem = (productId) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-
-      const { data } = await axios.put(
-        `/api/shop/orders/${productId}`,
-        {},
-        {
-          headers: {
-            authorization: token,
-          },
+export const getOrders = userId => {
+    return async dispatch => {
+        try {
+            if (token) {
+                const { data } = await axios.get(`/api/shop/orders/${userId}`, {
+                    headers: {
+                        authorization: token,
+                    },
+                });
+                dispatch(setOrders(data));
+            } else {
+                window.localStorage.setItem('order', []);
+                window.localStorage.getItem('order', []);
+            }
+        } catch (error) {
+            console.error(error);
         }
-      );
-      dispatch(addItem(data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    };
 };
 
-export const deleteOrderItem = (itemId) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-      await axios.delete(`/api/shop/orders/${itemId}`, {
-        headers: {
-          authorization: token,
-        },
-      });
-      dispatch(deleteItem(itemId));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export const addOrderItem = productId => {
+    return async dispatch => {
+        try {
+            const { data } = await axios.put(
+                `/api/shop/orders/${productId}`,
+                {},
+                {
+                    headers: {
+                        authorization: token,
+                    },
+                }
+            );
+            dispatch(addItem(data));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+};
+
+export const deleteOrderItem = itemId => {
+    return async dispatch => {
+        try {
+            await axios.delete(`/api/shop/orders/${itemId}`, {
+                headers: {
+                    authorization: token,
+                },
+            });
+            dispatch(deleteItem(itemId));
+        } catch (error) {
+            console.error(error);
+        }
+    };
 };
 
 export const changeQty = (itemId, num) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-      await axios.put(
-        '/api/shop/orders/qty',
-        { itemId, num },
-        {
-          headers: {
-            authorization: token,
-          },
+    return async dispatch => {
+        try {
+            await axios.put(
+                '/api/shop/orders/qty',
+                { itemId, num },
+                {
+                    headers: {
+                        authorization: token,
+                    },
+                }
+            );
+            dispatch(updateQty(itemId, num));
+        } catch (error) {
+            console.error(error);
         }
-      );
-      dispatch(updateQty(itemId, num));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    };
 };
 
 //INITIAL STATE
@@ -113,43 +113,44 @@ const initialState = [];
 
 //REDUCER
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case SET_ORDER:
-      return [...action.orders];
-    case ADD_ORDER_ITEM:
-      return state.map((order) =>
-        order.complete
-          ? order
-          : {
-            ...order,
-            lineItems: [...order.lineItems, action.item],
-          }
-      );
-    case DEL_ORDER_ITEM:
-      return state.map((order) =>
-        order.complete
-          ? order
-          : {
-            ...order,
-            lineItems: order.lineItems.filter(
-              (item) => item.id !== action.itemId
-            ),
-          }
-      );
-    case CHANGE_QTY:
-      const orderIndex = state.findIndex((order) => !order.complete);
-      const itemIndex = state[orderIndex].lineItems.findIndex(
-        (item) => action.itemId === item.id
-      );
-      if (
-        action.num <= 0 ||
-        action.num > state[orderIndex].lineItems[itemIndex].product.stock
-      )
-        return state;
-      const nextState = JSON.parse(JSON.stringify(state));
-      nextState[orderIndex].lineItems[itemIndex].qty = action.num;
-      return nextState;
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case SET_ORDER:
+            return [...action.orders];
+        case ADD_ORDER_ITEM:
+            return state.map(order =>
+                order.complete
+                    ? order
+                    : {
+                          ...order,
+                          lineItems: [...order.lineItems, action.item],
+                      }
+            );
+        case DEL_ORDER_ITEM:
+            return state.map(order =>
+                order.complete
+                    ? order
+                    : {
+                          ...order,
+                          lineItems: order.lineItems.filter(
+                              item => item.id !== action.itemId
+                          ),
+                      }
+            );
+        case CHANGE_QTY:
+            const orderIndex = state.findIndex(order => !order.complete);
+            const itemIndex = state[orderIndex].lineItems.findIndex(
+                item => action.itemId === item.id
+            );
+            if (
+                action.num <= 0 ||
+                action.num >
+                    state[orderIndex].lineItems[itemIndex].product.stock
+            )
+                return state;
+            const nextState = JSON.parse(JSON.stringify(state));
+            nextState[orderIndex].lineItems[itemIndex].qty = action.num;
+            return nextState;
+        default:
+            return state;
+    }
 };
