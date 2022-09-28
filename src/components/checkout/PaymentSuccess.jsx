f59ee112ec62122066f8984a88f7f00d953c3ead
nbenford <noel.benford@gmail.com>
2022-09-28 13:07:00 -0400

    confirm page update

--- src/components/checkout/PaymentSuccess.jsx
+++ src/components/checkout/PaymentSuccess.jsx
@@ -5,7 +5,7 @@ export default function PaymentSuccess() {
     <div>
       <h1>Thanks for your order!</h1>
       <p>
-        We appreciate your business! If you have any questions, please email
+        We appreciate your business! If you have any questions, please email{' '}
         <a href="mailto:orders@example.com">orders@example.com</a>.
       </p>
     </div>
