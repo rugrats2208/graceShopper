0981d49b8bb7d262433989b5130f0ac1dd6d370a
Andrew Dobson <tandrewlopez@gmail.com>
2022-09-28 21:14:43 -0400

    deployed...again.

--- server/db/models/LineItem.js
+++ server/db/models/LineItem.js
@@ -22,15 +22,15 @@ const LineItem = conn.define(
                 )
                     throw new Error('Item already exists in order');
             },
-            beforeSave: async lineItem => {
-                if (lineItem.qty < 1)
-                    throw new Error('Quantity cannot be less than 1');
-                const product = await lineItem.getProduct();
-                if (lineItem.qty > product.stock)
-                    throw new Error(
-                        'Quantity cannot be more than amount in stock'
-                    );
-            },
+            // beforeSave: async lineItem => {
+            //     if (lineItem.qty < 1)
+            //         throw new Error('Quantity cannot be less than 1');
+            //     const product = await lineItem.getProduct();
+            //     if (lineItem.qty > product.stock)
+            //         throw new Error(
+            //             'Quantity cannot be more than amount in stock'
+            //         );
+            // },
         },
     }
 );
